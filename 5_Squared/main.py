2438eb51b5af73c26a15eed6bd50df1f3ef1e25e
cristinalin <cristinalin2012@gmail.com>
2026-03-18 17:28:44 +0000

    added new alpha model from HRP

--- 5_Squared/main.py
+++ 5_Squared/main.py
@@ -5,6 +5,8 @@ from src.signals.ranker import Ranker
 from src.optimization.get_weights import Get_Weights
 from src.backtest.portfolio import Backtest
 
+import pandas as pd
+
 
 c = DataHandler()
 mo = Momentum()
@@ -13,5 +15,7 @@ w = Get_Weights()
 b = Backtest()
 
 b.run()
-print(w.opt_weights, w.opt_summary)
 print(w.weights)
+print(w.beta_penalized_weights, w.beta_penalized_summary)
+print(w.alpha_opt_weights,w.alpha_opt_summary)
+print(w.alpha_from_hrp_weights,w.alpha_from_hrp_summary)  
--- 5_Squared/src/optimization/portfolio_metrics.py
+++ 5_Squared/src/optimization/portfolio_metrics.py
@@ -23,7 +23,7 @@ class PortfolioMetrics:
 
         if self.bench_var <= 1e-12:
             raise ValueError("Benchmark variance is too close to zero.")
-
+    
     def portfolio_return(self, w, annualize):
         rp = float(w @ self.mu)
         return rp * self.periods_per_year if annualize else rp
@@ -67,7 +67,7 @@ class PortfolioMetrics:
     def benchmark_return(self, annualize):
         return self.bench_mean * self.periods_per_year if annualize else self.bench_mean
 
-    def summary(self, w, beta_penalty, annualize):
+    '''def summary(self, w, beta_penalty, annualize):
         sharpe = self.sharpe_ratio(w, annualize=annualize)
         beta = self.portfolio_beta(w)
 
@@ -79,4 +79,19 @@ class PortfolioMetrics:
             "Beta": float(beta),
             "Implied Excess Returns vs benchmark (S&P 500 Index)": self.implied_alpha(w, annualize=annualize),
             "Objective Value": float(-self.sharpe_ratio(w, annualize=annualize) + beta_penalty * beta),
+        }'''
+    
+    def summary(self, w: np.ndarray, objective_value: float | None = None, annualize: bool = True) -> dict:
+        summary = {
+            "Expected Return of the Portfolio": self.portfolio_return(w, annualize=annualize),
+            "Expected Return of the Benchmark (S&P 500 Index)": self.benchmark_return(annualize=annualize),
+            "Volatility": self.portfolio_std(w, annualize=annualize),
+            "Sharpe Ratio": self.sharpe_ratio(w, annualize=annualize),
+            "Beta": self.portfolio_beta(w),
+            "Alpha vs benchmark (S&P 500 Index)": self.implied_alpha(w, annualize=annualize),
         }
+
+        if objective_value is not None:
+            summary["Objective Value"] = float(objective_value)
+
+        return summary
